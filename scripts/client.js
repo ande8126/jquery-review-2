d535de93ac28ae168df8641e8cfbe96c7ebc451a
Pat Anderson <patrick.b.anderson@gmail.com>
2021-03-05 11:01:34 -0600

    updating js with jquery to add animals to DOM

--- scripts/client.js
+++ scripts/client.js
@@ -1,5 +1,31 @@
 $( document ).ready( onReady );
+let zoo = [];
 
 function onReady() {
-    console.log('JQ');
+    //button click
+    $( '#addAnimalButton' ).on( 'click', addAnimal )
 }//end onReady
+
+function addAnimal(){
+    console.log( 'in addAnimal' );
+    //push animal into zoo
+    zoo.push( $('#animalIn' ).val());
+    $( '#animalIn' ).val( '' );
+    //update DOPM<
+    updateAnimals();
+}//end addAnimal
+
+function updateAnimals(){
+    //target ul by id
+    let el = $( '#animalsOut' );
+    //empty list
+    el.empty();
+    //loop thru the zoo
+    for ( let i=0; i<zoo.length; i++ ) {
+    //append each animal to the DOM
+        el.append( `<li>${zoo[i]}</li>`);
+    }//end updateAnimals
+
+
+
+}//end updateAnimals
